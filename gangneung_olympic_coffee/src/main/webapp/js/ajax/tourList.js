0a8b41dba44c38bcf906794103ad4a356af5cb78
sinseung-yeob <sinseung-yeob@192.168.0.2>
2017-12-27 14:06:14 +0900

    2017.12.27 - 관광지 모달 폼 수정

--- gangneung_olympic_coffee/src/main/webapp/js/ajax/tourList.js
+++ gangneung_olympic_coffee/src/main/webapp/js/ajax/tourList.js
@@ -4,9 +4,8 @@
 
 var slideIndex = 1;
 var tourListRequest;
-//showDivs(slideIndex);
-///tourList.do
 function tourList(){
+	document.getElementById('touristList').innerHTML='';
 	tourListRequest = sendRequest("tourList.do", null, tourListAjax, "POST");
 }
 
@@ -19,10 +18,10 @@ function tourListAjax() {
 				'columnId' :['name'],
 				'columnClass' : ['name cursor'],
 				'innerSet' : {
-						'hiddenColumn' : ['id','tourSource'],
+						'hiddenColumn' : ['name','id','tourSource'],
 						'divClass' : '',
 						'divId' : '',
-						'tableClass' : 'store-list pic col-xs-6 col-ms-4 col-md-4 col-lg-4',
+						'tableClass' : 'tour-list pic col-xs-6 col-ms-4 col-md-4 col-lg-4',
 						'tableId' : ''
 				},
 				'innerFunction' : tourSelectById,
@@ -33,12 +32,31 @@ function tourListAjax() {
 
 function tourSelectById(element){
 	element.addEventListener("click",function(){
+		slideIndex = 1;
 		var name = elementChildSelectorName(element,'name').value;
 		var source = elementChildSelectorName(element,'tourSource').value;
+		source +='<a class="btn-floating" style="position:absolute;top:45%;left:0" onclick="plusDivs(-1)">❮</a><a class="btn-floating" style="position:absolute;top:45%;right:0" onclick="plusDivs(1)">❯</a>';
 		$(document).ready(function() {
 			var modalView = document.getElementById('store-modal-view');
-			modalView.innerHTML=source;
+			modalView.innerHTML='';
+			var innerModalView = document.createElement('div');
+			innerModalView.className = "tour-modal";
+			innerModalView.id = "tour-modal";
+			innerModalView.innerHTML=source;
+			
+			var tourName = document.createElement('div');
+			tourName.className = "tour-name";
+			tourName.id = "tour-name";
+			tourName.appendChild(document.createTextNode(name));
+			
+			iconMaker(tourName, 'tour_icon', 'img/info.svg', function(e){
+				window.open('https://www.google.com/search?q='+name);
+			});
+			
+			modalView.appendChild(tourName);
+			modalView.appendChild(innerModalView);
 			$("#store-modal").modal();
+			showDivs(slideIndex);
 		});
 	});
 }
@@ -49,7 +67,7 @@ function plusDivs(n) {
 
 function showDivs(n) {
     var i;
-    var x = document.getElementsByClassName("mySlides");
+    var x = document.getElementById('tour-modal').getElementsByTagName("img");
     if (n > x.length) {slideIndex = 1} 
     if (n < 1) {slideIndex = x.length} ;
     for (i = 0; i < x.length; i++) {
